7ce8026720e989150de0f1cde09cf0b25f5b9d56
riskyprsty <66415817+riskyprsty@users.noreply.github.com>
2024-02-09 10:57:13 +0700

    refactor: add linter and format

--- server/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-import mountainRoutes from './mountains.js'
-
-const router = express.Router()
-
-router.use('/mountains', mountainRoutes(router));
-
-export default router;
